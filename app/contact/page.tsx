0a34a4a9abbc088742fd0c7cf446e888d8309a65
Paul <Paul@N6-Pharma.local>
2025-06-12 08:11:47 +0200

    Add contact page, modify services page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,13 +8,12 @@ export const metadata: Metadata = {
 export default function ContactPage() {
   return (
     <section className="mx-auto max-w-3xl px-4 py-16 text-center sm:px-6 lg:px-8">
-      <h1 className="font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-5xl md:text-6xl">
+      <h1 className="font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl dark:text-white">
         Let’s Connect
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg text-gray-600 dark:text-gray-300">
         If you're interested in working together, hiring me, or simply getting in touch, feel free to schedule a call at your convenience.
       </p>
-
       <a
         href="https://calendly.com/paul-peyssard"
         target="_blank"
@@ -23,8 +22,6 @@ export default function ContactPage() {
       >
         Book a Call
       </a>
-
-
     </section>
   )
 }
